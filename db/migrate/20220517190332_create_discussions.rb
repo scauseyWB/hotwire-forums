0341d82c38fe13ddf20355a35c8d8a66c6f116c3
Samantha Causey <scausey@wrapbook.com>
2022-05-17 15:11:15 -0400

    Forgot to save discussion create migration

--- db/migrate/20220517190332_create_discussions.rb
+++ db/migrate/20220517190332_create_discussions.rb
@@ -2,8 +2,8 @@ class CreateDiscussions < ActiveRecord::Migration[6.1]
   def change
     create_table :discussions do |t|
       t.string :name
-      t.boolean :pinned
-      t.boolean :closed
+      t.boolean :pinned, default: false
+      t.boolean :closed, default: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
